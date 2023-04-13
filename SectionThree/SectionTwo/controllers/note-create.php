5136f07c842240aa3add0763c19a9e0c4abfcd29
matt190589 <matthew.doherty97@gmail.com>
2023-04-13 21:30:03 +0100

    Validation on the notes box added on the client side with error message and on the client side with  variable

--- SectionThree/SectionTwo/controllers/note-create.php
+++ SectionThree/SectionTwo/controllers/note-create.php
@@ -6,12 +6,24 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1,
-    ]);
-}
 
-// dd($_SERVER);
+    $ERRORS = [];
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+
+    if (strlen($_POST['body']) > 100) {
+        $errors['body'] = 'Message too long!!!';
+    }
+
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
+}
 
 require('views/note-create.view.php');
