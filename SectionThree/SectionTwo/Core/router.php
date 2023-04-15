0fdec73b92f208d2740c70f6b9aa180e4e6ca379
matt190589 <matthew.doherty97@gmail.com>
2023-04-15 21:58:35 +0100

    Namespace added related to Core files

--- SectionThree/SectionTwo/Core/router.php
+++ SectionThree/SectionTwo/Core/router.php
@@ -1,12 +1,12 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -16,7 +16,7 @@ function abort($code = 404)
 {
     http_response_code($code);
 
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
 
     die();
 }
