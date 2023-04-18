cd165a7ec6e52ca9316234167a19ea23ee4778eb
matt190589 <matthew.doherty97@gmail.com>
2023-04-18 20:41:04 +0100

    Routes.php updated to allow delte requests on notes and post requests on the creat note form

--- SectionThree/SectionTwo/controllers/notes/destroy.php
+++ SectionThree/SectionTwo/controllers/notes/destroy.php
@@ -6,15 +6,18 @@ $config = require base_path('config.php');
 $db = new Database($config['database']);
 $currentUserId = 1;
 
+
 $note = $db->query('SELECT * FROM notes where id = :id', [
-    'id'  => $_GET['id']
+    'id'  => $_POST['id']
 ])->findOrFail();
 
 
 authorize($note['user_id'] === $currentUserId);
 
+$db->query('DELETE FROM notes where id = :id', [
+    'id' => $_POST['id']
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
 ]);
+
+header('location: /notes');
+exit();
