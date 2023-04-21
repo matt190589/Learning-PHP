5ede4fbc4a23738885950c67219e090b13c2afdd
matt190589 <matthew.doherty97@gmail.com>
2023-04-21 19:03:14 +0100

    Edit.view added to allow existing notes to be changed. Patch request to be added to DB

--- SectionThree/SectionTwo/views/notes/show.view.php
+++ SectionThree/SectionTwo/views/notes/show.view.php
@@ -6,14 +6,20 @@
         <p>
             <?= htmlspecialchars($note['body']) ?>
         </p>
+
+        <footer class="mt-6">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
+        </footer>
+
+
         <button class="mt-6">
             <a href="/notes" class="text-blue-500 underline">Go Back</a>
         </button>
-        <form method="post">
+        <!-- <form method="post">
             <input type="hidden" name="_method" value="delete">
             <input type='hidden' name='id' value="<?= $note['id'] ?>">
             <button class="mt-2 text-sm text-red-500">Delete</button>
-        </form>
+        </form> -->
     </div>
 </main>
 
